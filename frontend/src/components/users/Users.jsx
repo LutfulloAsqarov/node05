4851b1d3574a97ad856eeac59979a6f4b5bfbfc2
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-08-01 20:58:22 +0500

    update

--- frontend/src/components/users/Users.jsx
+++ frontend/src/components/users/Users.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetUsersQuery } from "../../context/api/usersApi";
 import "./users.css";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const Users = () => {
-    const { data } = useGetUsersQuery();
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(3);
+    // const [skip, setSkip] = useState(1);
+
+    const { data } = useGetUsersQuery({
+        skip: page,
+    });
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+
+    let pageCount = Math.floor(data?.total % limit) || 0;
+    console.log(pageCount);
 
     console.log(data);
     let usersItems = data?.payload?.map((user) => (
@@ -20,9 +36,20 @@ const Users = () => {
     ));
 
     return (
-        <div className="users">
-            <div className="users__cards">{usersItems}</div>
-        </div>
+        <>
+            <div className="users">
+                <div className="users__cards">{usersItems}</div>
+                <div className="pagination">
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={pageCount}
+                            page={page}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                </div>
+            </div>
+        </>
     );
 };
 
